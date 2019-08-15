7ad8619865fd8c12a13cf498dea7cdcfe7bf2fd8
Tyler Turnipseed <tyler@techturnip.us>
2019-08-14 20:20:14 -0400

    added env variables

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const server = require('./api/server.js');
+// this import should always be first
+require('dotenv').config()
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
-});
+const server = require('./api/server.js')
+
+const port = process.env.PORT || 4000
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on Port ${port} ***\n`)
+})
+
+// when running locally the IP is taken care of by localhost
+// and we can hardcode any old port (over 3000) in
+
+// when deploying
+// heroku will assign an IP address
+// port cannot be hardcoded in
+// heroku needs to tell us which port
+// (because it may be taken)
